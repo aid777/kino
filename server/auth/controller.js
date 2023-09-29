b87bc614d2ecb9aee5bfec10f0a2f7fd6f0e110b
aid777 <aidar.abilkair@icloud.com>
2023-09-29 20:32:13 +0600

    fail3

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -1,33 +1,8 @@
-const User = require('./User')
+const User = require('./user')
 const bcrypt = require('bcrypt')
 
 
 const signUp = async(req , res) =>{
-    // if(
-    //     req.body.email.length <= 0 && 
-    //     req.body.full_name.length <= 0 &&
-    //     req.body.password.length <= 0 &&
-    //     req.body.re_password.length <= 0
-    //     ){
-    //         res.redirect('register?error=1')
-    //     }else if(req.body.password !== req.body.re_password){
-    //         res.redirect('/register?error=2')
-    //     }
-    //     const findUser = await User.findOne({email :  req.body.email}).count()
-    //     console.log(findUser);
-    //     if(findUser){
-    //         res.redirect('/register?error=3') 
-    //     }
-    //     bcrypt.genSalt(10, (err, salt) => {
-    //         bcrypt.hash(req.body.password, salt, function(err, hash) {
-    //             new User({
-    //                 email:  req.body.email,
-    //                 full_name:  req.body.full_name,
-    //                 password: hash
-    //             }).save();
-    //             res.redirect('/login')
-    //         });
-    //     })
     if(
         req.body.email.length > 0 &&
         req.body.full_name.length > 0 &&
@@ -57,6 +32,12 @@ const signUp = async(req , res) =>{
         res.redirect('/register?error=1');
     }
     }
+
+const signIn = ( req, res) => {
+    res.redirect('/profile')
+}
+
 module.exports = {
-    signUp
+    signUp,
+    signIn
 }
---
+++ server/config/passport.js
@@ -0,0 +1,14 @@
+const passport = require('passport')
+const User = require('./../auth/User')
+const bcrypt = require('bcrypt')
+const LocalStrategy = require('passport-local')
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email',
+    },
+    function(email , passpword , done) {
+        console.log(email);
+        console.log(password);
+    }
+))
